03760a665f9a9f2fac4a6a1eb8e6e0dddd17d6a8
Dhiraj Saharia <dsaharia@outlook.com>
2019-11-08 23:04:49 +0530

    manager php file

---
+++ manager.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Manager Page</title>
+</head>
+<body>
+    <h2><b>Welcome Manager</b></h2>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
