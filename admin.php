d3c8100704c86b1acd333a80e617a27014132695
Dhiraj Saharia <dsaharia@outlook.com>
2019-11-19 15:17:38 +0530

    css added basics and centred blocks

---
+++ admin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="admin.css">
+    <title class="title">Admin Page</title>
+</head>
+<body>
+    <h3>Insert Data </h3>
+     <div class="dropdown">
+          <button class="dropbtn">Select Table</button>
+          <div class="dropdown-content">
+            <a href="insert_emp.php">Employees</a>
+            <a href="insert_dept.php">Department</a>
+            <a href="insert_location.php">Location</a>
+            <a href="insert_product.php">Product</a>
+            <a href="insert_dependent.php">Dependent</a>
+            <a href="insert_emp_product.php">Employee-Product</a>
+          </div>
+    </div> 
+</body>
+</html>
