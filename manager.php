d9926aea1b2cb61e0b9507590a854d6cac8bf874
Dhiraj Saharia <dsaharia@outlook.com>
2019-11-19 15:17:56 +0530

    manager php file added, centered

--- manager.php
+++ manager.php
@@ -2,10 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Manager Page</title>
+    <title >Manager Page</title>
+    <link rel="stylesheet" type="text/css" href="manager.css">
 </head>
 <body>
-    <h2><b>Welcome Manager</b></h2>
+    <h2 class="header"><b><u>Welcome Manager</u></b></h2><hr>
+    <div class="vertical-menu">
+        <a class="active">Select a Database View</a>
+        <a href="all_emp.php">All Employees in a department</a>
+        <a href="#">Products under a department</a>
+        <a href="#">Link 3</a>
+        <a href="#">Link 4</a>
+    </div> 
+    <br>
     <a href="logout.php">Logout</a>
 </body>
 </html>
