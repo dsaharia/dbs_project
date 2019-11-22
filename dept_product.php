75f670c5cc09d040c9b0bb09c8e19d47b8335bf9
Dhiraj Saharia <dsaharia@outlook.com>
2019-11-22 22:36:59 +0530

    department product view added

--- dept_product.php
+++ dept_product.php
@@ -1,12 +1,11 @@
 <?php 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $connection = mysqli_connect('localhost', 'root', '', "dbs_project") or die("Could Not connect to DB");
 
-    $h_id = (int)mysqli_real_escape_string($connection, $_POST['h_id']);
 
-    $get_user_sql = "SELECT P.Pname,P.Budget,P.St_date,P.Totalsales,D.Dnumber from PRODUCT P JOIN DEPT D ON(P.D_NO = D.Dnumber) where P.Head_id = $h_id ";
+    $get_user_sql = "SELECT P.Pname,P.Budget,P.St_date,P.Totalsales,D.Dnumber,E.Fname,E.Lname,P.Head_id from PRODUCT P, EMP E, DEPT D where P.D_NO=D.Dnumber and E.ID=P.Head_id";
+
     if ($result = mysqli_query($connection, $get_user_sql)) {
-        echo "<br>"."<b>Query successfull</b>"."<br>";
+        echo "<br>"."<b>Query successfull</b>"."<br>"."<br>";
         echo "<table>"; 
         echo "<tr>"; 
         echo "<th>Project Name</th>"; 
@@ -14,6 +13,9 @@
         echo "<th>Start Date</th>";
         echo "<th>Total Sales</th>";
         echo "<th>Dept. Number</th>";
+        echo "<th>Fname</th>";
+        echo "<th>Lname</th>";
+        echo "<th>Product Head ID</th>";
         echo "</tr>";
         while($row=mysqli_fetch_assoc($result)){
             echo "<tr>"; 
@@ -22,6 +24,9 @@
                 echo "<td>".$row['St_date']."</td>";
                 echo "<td>".$row['Totalsales']."</td>";
                 echo "<td>".$row['Dnumber']."</td>";
+                echo "<td>".$row['Fname']."</td>";
+                echo "<td>".$row['Lname']."</td>";
+                echo "<td>".$row['Head_id']."</td>";
             echo "</tr>"; 
         }
     }
@@ -29,7 +34,7 @@
         echo "Error: " . "<br>" . mysqli_error($connection);
     }
     mysqli_close($connection);
- }
+ 
  ?>
 
 
@@ -39,11 +44,5 @@
     <title>Views</title>
 </head>
 <body>
-    <br>
-    <form action="" method="POST">
-        <label for="did"><b>Enter Manager. ID: </b></label>
-        <input type="text" name="h_id" required><br><br>
-        <input type="submit" value="Log in"><br><br>
-    </form>
 </body>
 </html>
