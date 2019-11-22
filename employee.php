992eaefc7c1493e7b55364d04a321856bbb91107
Dhiraj Saharia <dsaharia@outlook.com>
2019-11-22 17:29:59 +0530

    employee data

---
+++ employee.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title >Employee Page</title>
+    <link rel="stylesheet" type="text/css" href="manager.css">
+</head>
+<body>
+    <h2 class="header"><b><u>Welcome Employee</u></b></h2><hr>
+    <div class="vertical-menu">
+        <a class="active">Select a Database View</a>
+        <a href="all_emp.php">All Employees in a department</a>
+        <a href="#">Products under a department</a>
+        <a href="#">Link 3</a>
+        <a href="#">Link 4</a>
+    </div> 
+    <br>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
