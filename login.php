e5f572e39c8c8236f10ac3d31a219221d5a32476
Dhiraj Saharia <dsaharia@outlook.com>
2019-11-22 17:30:09 +0530

    sql comments added

--- login.php
+++ login.php
@@ -1,6 +1,13 @@
 <?php 
 session_start();
-
+// SELECT E.Fname,E.Lname,E.Minit,E.Phone,E.Post,D.Name from EMP E JOIN DEPT D ON(E.D_ID = D.Dnumber) done
+//SELECT D.Name,D.Relatntype from EMP E JOIN DEPENDENT D ON (E.ID = D.E_ID)
+//SELECT E.Fname,E.Lname,E.Minit,E.Minit,E.Phone,E.Salary from EMP E JOIN DEPT D ON (E.D_ID = D.Dnumber) where D.Mgr = 4
+//SELECT P.Pname,P.Budget,P.St_date,P.Totalsales,D.Dnumber from PRODUCT P JOIN DEPT D ON(P.D_NO = D.Dnumber) where P.Head_id = 2 
+//SELECT E.Fname,E.Lname,E.Minit,P.D_NO from EMP E JOIN PRODUCT P on(P.Head_id = E.ID) where P.D_NO = 2 
+//SELECT D_ID, COUNT(*) FROM EMP GROUP BY D_ID 
+//SELECT Name,Dnumber,Website from DEPT 
+//SELECT Sname,Buildname,City,Country from LOCATION where D_no = 2 
 $servername = "localhost";
 
 $connection = mysqli_connect($servername, 'root', '', "dbs_project") or die("Could Not connect to DB");
